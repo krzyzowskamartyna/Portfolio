b1b03fb964a061c93e08587f51098353fa18143c
krzyzowskamartyna <krzyzowskamartyna@gmail.com>
2018-12-29 10:10:24 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
 import Start from './components/Start';
-import './css/App.css';
+import Navbar from './components/Navbar';
 
+import './css/App.css';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Start />
+        <Navbar />
       </div>
     );
   }
--- src/components/Start.js
+++ src/components/Start.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
-import Konva from "konva";
-import { render } from "react-dom";
-import { Stage, Layer, Rect, Text } from "react-konva";
+import Particles from 'react-particles-js';
+import Icon from '@material-ui/core/Icon';
+import Button from '@material-ui/core/Button';
 
 import '../css/start.css';
+
 
 class Start extends Component {
     render() {
         return (
             <div className="start">
-                <div className="title">
-                    <h1> Martyna <span className="title-sec">Krzyżowska</span></h1>
-                    <h2>front end developer</h2>
+                <div>
+
+                    <div className="title">
+                        <h1> Martyna <span className="title-sec">Krzyżowska</span></h1>
+                        <h2>I'm a front end developer</h2>
+                        <Button variant="outlined" color="secondary" className="btn">
+                            Show more  <Icon className='fas fa-arrow-right'></Icon>
+                        </Button>
+                    </div>
+                    <Particles />
                 </div>
-            </div>
+
+            </div >
         )
     }
 };
